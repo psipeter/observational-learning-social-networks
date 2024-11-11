5f93ee424e490a42420d68a3e0b224cd26725f70
Peter Duggins <psipeter@gmail.com>
2024-11-11 14:16:49 -0500

    cleanup jobs script

--- batch/make_jobs.py
+++ batch/make_jobs.py
@@ -1,7 +1,7 @@
 import sys
 import pandas as pd
 
-z = sys.argv[1]
+z = float(sys.argv[1])
 sids = pd.read_pickle("data/behavior.pkl")['sid'].unique()
 
 for sid in sids:
@@ -14,4 +14,5 @@ for sid in sids:
       rsh.write('''#SBATCH --ntasks-per-node=1''')
       rsh.write("\n")
       rsh.write('''#SBATCH --time=0:20:0''')
+      rsh.write("\n")
       rsh.write(string)
