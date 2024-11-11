a8c663cd5d9be0e2bc5228019e95a56c25308af2
Peter Duggins <psipeter@gmail.com>
2024-11-11 14:14:17 -0500

    make jobs script

---
+++ batch/make_jobs.py
@@ -0,0 +1,17 @@
+import sys
+import pandas as pd
+
+z = sys.argv[1]
+sids = pd.read_pickle("data/behavior.pkl")['sid'].unique()
+
+for sid in sids:
+   string = f"python run.py WM {sid} {z}"
+   with open (f'WM_{sid}.sh', 'w') as rsh:
+      rsh.write('''#!/bin/bash''')
+      rsh.write("\n")
+      rsh.write('''#SBATCH --nodes=1''')
+      rsh.write("\n")
+      rsh.write('''#SBATCH --ntasks-per-node=1''')
+      rsh.write("\n")
+      rsh.write('''#SBATCH --time=0:20:0''')
+      rsh.write(string)
