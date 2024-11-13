37a0b96d0226b2c805036a442e76e63b27c2e8c9
Peter Duggins <psipeter@gmail.com>
2024-11-13 11:13:57 -0500

    delete slurm files in submit_jobs

--- submit_jobs.py
+++ submit_jobs.py
@@ -5,11 +5,11 @@ import time
 
 ## Working Memory
 sids = pd.read_pickle("data/behavior.pkl")['sid'].unique()
+slurm_string = ["rm", f"*.out"]
+c = subprocess.run(slurm_string)
+
 for sid in sids:
    delete_string = ["rm", f"data/wm_{sid}.pkl"]
-   delete_string2 = ["rm", f"slurm-*"]
    submit_string = ["sbatch", f"wm_{sid}.sh"]
    a = subprocess.run(delete_string)
    b = subprocess.run(submit_string)
-   c = subprocess.run(delete_string2)
-   # time.sleep(0.5)
