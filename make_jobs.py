0f560e88885eaf745658db072a6fcd53465bff70
Peter Duggins <psipeter@hydra.dartmouth.edu>
2024-11-20 12:20:30 -0500

    proper indentation in make_jobs

--- make_jobs.py
+++ make_jobs.py
@@ -5,16 +5,15 @@ import subprocess
 model_type = sys.argv[1]
 sids = pd.read_pickle("data/behavior.pkl")['sid'].unique()
 
-if model_type in ['NEF-WM', 'NEF-RL', 'RL1', 'RL1rd', 'RL2', 'RL2rd']:
-   make_string = f"python fit.py {model_type} {sid}"
-   file_string = f'fit_{sid}.sh'
-if model_type in ['WM', 'RL']:
-   z = float(sys.argv[2])
-   k = float(sys.argv[3])
-   make_string = f"python run.py {model_type} {sid} {z} {k}"
-   file_string = f'nef_{sid}.sh'
-
 for sid in sids:
+   if model_type in ['NEF-WM', 'NEF-RL', 'RL1', 'RL1rd', 'RL2', 'RL2rd']:
+      make_string = f"python fit.py {model_type} {sid}"
+      file_string = f'fit_{sid}.sh'
+   if model_type in ['WM', 'RL']:
+      z = float(sys.argv[2])
+      k = float(sys.argv[3])
+      make_string = f"python run.py {model_type} {sid} {z} {k}"
+      file_string = f'nef_{sid}.sh'
    with open (file_string, 'w') as rsh:
       rsh.write('''#!/bin/bash''')
       rsh.write("\n")
