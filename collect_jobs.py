53ab39f53e070e623d4c7ebc6f93ab33f423dbcc
Peter Duggins <psipeter@hydra.dartmouth.edu>
2024-11-25 17:03:34 -0500

    typo

--- collect_jobs.py
+++ collect_jobs.py
@@ -13,7 +13,7 @@ for sid in sids:
 	try:
 		if model_type in ['all', 'NEF-WM', 'NEF-RL', 'RL1', 'RL3', 'RL3rd', 'ZK', 'DGn', 'DGrds']:
 			if model_type == 'all':
-				for mt in ['RL1', 'RL3rd', 'DGn', 'DGrds', 'ZK', 'NEF-WM', 'NEF-RL']
+				for mt in ['RL1', 'RL3rd', 'DGn', 'DGrds', 'ZK', 'NEF-WM', 'NEF-RL']:
 					df1 = pd.read_pickle(f"data/{mt}_{sid}_performance.pkl")
 					df2 = pd.read_pickle(f"data/{mt}_{sid}_param.pkl")
 					df3 = pd.read_pickle(f"data/{mt}_{sid}_rerun.pkl")
