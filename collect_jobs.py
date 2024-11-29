6edd2d73ee10a0db2d92a378c063d0da6c7d61ad
Peter Duggins <psipeter@gmail.com>
2024-11-29 10:18:20 -0500

    reorganize try/except in collect_jobs

--- collect_jobs.py
+++ collect_jobs.py
@@ -9,36 +9,43 @@ dfs1 = []
 dfs2 = []
 dfs3 = []
 
-for sid in sids:
-	try:
-		if model_type in ['all', 'NEF-WM', 'NEF-RL', 'RL1', 'RL3', 'RL3rd', 'ZK', 'DGn', 'DGrd', 'DGrds']:
-			if model_type == 'all':
-				for mt in ['RL1', 'RL3rd', 'DGn', 'DGrd', 'DGrds', 'ZK', 'NEF-WM', 'NEF-RL']:
+if model_type in ['all', 'NEF-WM', 'NEF-RL', 'RL1', 'RL3', 'RL3rd', 'ZK', 'DGn', 'DGrd', 'DGrds']:
+	if model_type == 'all':
+		for mt in ['RL1', 'RL3rd', 'DGn', 'DGrd', 'DGrds', 'ZK', 'NEF-WM', 'NEF-RL']:
+			for sid in sids:
+				try:
 					df1 = pd.read_pickle(f"data/{mt}_{sid}_performance.pkl")
 					df2 = pd.read_pickle(f"data/{mt}_{sid}_params.pkl")
 					df3 = pd.read_pickle(f"data/{mt}_{sid}_rerun.pkl")
 					dfs1.append(df1)
 					dfs2.append(df2)
 					dfs3.append(df3)
-			else:
+				except:
+					print(f"missing {mt} {sid}")
+		dfs3.append(pd.read_pickle("data/human.pkl"))
+	else:
+		for sid in sids:
+			try:
 				df1 = pd.read_pickle(f"data/{model_type}_{sid}_performance.pkl")
 				df2 = pd.read_pickle(f"data/{model_type}_{sid}_params.pkl")
 				df3 = pd.read_pickle(f"data/{model_type}_{sid}_rerun.pkl")
 				dfs1.append(df1)
 				dfs2.append(df2)
 				dfs3.append(df3)
-		if model_type in ['WM', 'RL']:
-			df1 = pd.read_pickle(f"data/{model_type}_{sid}.pkl")
-			dfs1.append(df1)
+			except:
+				print(f"missing {mt} {sid}")
+	performance = pd.concat(dfs1, ignore_index=True)
+	params = pd.concat(dfs2, ignore_index=True)
+	reruns = pd.concat(dfs3, ignore_index=True)
+	performance.to_pickle(f"data/{model_type}_{label}_performance.pkl")
+	params.to_pickle(f"data/{model_type}_{label}_params.pkl")
+	reruns.to_pickle(f"data/{model_type}_{label}_reruns.pkl")
+
+if model_type in ['WM', 'RL']:
+	try:
+		df1 = pd.read_pickle(f"data/{model_type}_{sid}.pkl")
+		dfs1.append(df1)
 	except:
-		print(f"sid {sid} missing")
-
-if model_type == 'all':
-	dfs3.append(pd.read_pickle("data/human.pkl"))
-
-performance = pd.concat(dfs1, ignore_index=True)
-params = pd.concat(dfs2, ignore_index=True)
-reruns = pd.concat(dfs3, ignore_index=True)
-performance.to_pickle(f"data/{model_type}_{label}_performance.pkl")
-params.to_pickle(f"data/{model_type}_{label}_params.pkl")
-reruns.to_pickle(f"data/{model_type}_{label}_reruns.pkl")
+		print(f"missing {model_type} {sid}")
+	performance = pd.concat(dfs1, ignore_index=True)
+	performance.to_pickle(f"data/{model_type}_{label}_performance.pkl")
