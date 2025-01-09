9e16aa62f970c113a138a0bfe51c6b0de576d02a
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-09 17:04:05 -0500

    remove old models from fit.py; get_expectations() now returns all previous expectations, and the most recent one is fetched from this list

--- fit.py
+++ fit.py
@@ -17,72 +17,32 @@ def compute_mcfadden(NLL, sid):
             prob = scipy.special.expit(0)
             null_log_likelihood -= np.log(prob) if act==1 else np.log(1-prob)
     mcfadden_r2 = 1 - NLL/null_log_likelihood
-    # n_trials = len(subdata['trial'].unique()) * len(subdata['stage'].unique())
-    # mcfadden_r2 = 1 - NLL/(n_trials*np.log(0.5))
     return mcfadden_r2
 
 def get_param_names(model_type):
-    if model_type in ['NEF_WM', 'NEF_RL']:
-        param_names = ['type', 'sid', 'z', 'k', 'inv_temp']
-    if model_type == 'RLz':
+    if model_type in ['RLz', 'NEF_RL']:
         param_names = ['type', 'sid', 'z', 'b', 'inv_temp']
-    if model_type == 'RLzx':
-        param_names = ['type', 'sid', 'z', 'b', 'inv_temp']
-    if model_type == 'RL1':
-        param_names = ['type', 'sid', 'learning_rate_1', 'inv_temp']
-    if model_type == 'RL3':
-        param_names = ['type', 'sid', 'learning_rate_1', 'learning_rate_2', 'learning_rate_3', 'inv_temp']
-    if model_type == 'RL3rd':
-        param_names = ['type', 'sid', 'learning_rate_1', 'learning_rate_2', 'learning_rate_3', 'inv_temp']
-    if model_type in ['Z0K1']:
-        param_names = ['type', 'sid', 'inv_temp']
-    if model_type == 'Z':
-        param_names = ['type', 'sid', 'z', 'inv_temp']
-    if model_type == 'K':
-        param_names = ['type', 'sid', 'k', 'inv_temp']
-    if model_type == 'ZK':
+    if model_type in ['DGzk', 'NEF_WM']:
         param_names = ['type', 'sid', 'z', 'k', 'inv_temp']
-    if model_type == 'ZxK':
-        param_names = ['type', 'sid', 'z', 'k', 'inv_temp']
-    if model_type == 'ZS':
-        param_names = ['type', 'sid', 's1', 's2', 's3', 'z', 'inv_temp']
-    if model_type == 'DGn':
-        param_names = ['type', 'sid', 'inv_temp']
-    if model_type == 'DGrd':
-        param_names = ['type', 'sid', 'inv_temp']
-    if model_type == 'DGrds':
-        param_names = ['type', 'sid', 's2', 's3', 'inv_temp']
-    if model_type == 'DGrdp':
-        param_names = ['type', 'sid', 'inv_temp']
-    if model_type == 'DGrdpz':
-        param_names = ['type', 'sid', 'z', 'inv_temp']
     return param_names
 
-def get_expectation(model_type, params, trial, stage, sid):
+def get_param_init_bounds(model_type):
+    if model_type in ['RLz', 'NEF_RL']:
+        param0 = [0.1, 0.1, 1.0]
+        bounds = [(0,3), (0,1), (0,30)]
+    if model_type in ['DGzk', 'NEF_WM']:
+        param0 = [0.5, 1.0, 1.0]
+        bounds = [(0,3), (0.1,5), (0,30)]
+    return param0, bounds
+
+def get_expectations(model_type, params, trial, stage, sid):
     human = pd.read_pickle(f"data/human.pkl").query("sid==@sid")
     if model_type == 'NEF_WM':
         nef_data = pd.read_pickle(f"data/NEF_WM_{sid}_estimates.pkl")
-        expectation = nef_data.query("trial==@trial & stage==@stage")['estimate'].to_numpy()[-1]
+        expectations = nef_data.query("trial==@trial & stage<=@stage")['estimate'].to_numpy()
     if model_type == 'NEF_RL':
         nef_data = pd.read_pickle(f"data/NEF_RL_{sid}_estimates.pkl")
-        expectation = nef_data.query("trial==@trial & stage==@stage")['estimate'].to_numpy()[-1]
-    if model_type in ['RL1', "RL3", "RL3rd"]:
-        if model_type == 'RL1':
-            learning_rates = [1, params[0], params[0], params[0]]
-        if model_type in ['RL3', 'RL3rd']:
-            learning_rates = [1, params[0], params[1], params[2]]
-        subdata = human.query("trial==@trial & stage<=@stage")
-        observations = subdata['color'].to_numpy()
-        RDs = subdata['RD'].to_numpy()
-        expectation = 0
-        for o, obs in enumerate(observations):
-            stg = int(subdata.iloc[o]['stage'])
-            RD = RDs[o] if (model_type in ['RL3rd'] and stg>1) else 1
-            learning_rate = learning_rates[stg]
-            error = obs - expectation
-            LR = RD*learning_rate
-            LR = np.clip(LR, 0, 1)
-            expectation += LR * error
+        expectations = nef_data.query("trial==@trial & stage<=@stage")['estimate'].to_numpy()
     if model_type == 'RLz':
         z = params[0]
         b = params[1]
@@ -91,6 +51,7 @@ def get_expectation(model_type, params, trial, stage, sid):
         observations = subdata['color'].to_numpy()
         RDs = subdata['RD'].to_numpy()
         expectation = 0
+        expectations = []
         for o, obs in enumerate(observations):
             stg = int(subdata.iloc[o]['stage'])
             RD = 0 if stg<2 else RDs[o]
@@ -99,39 +60,15 @@ def get_expectation(model_type, params, trial, stage, sid):
             LR = learning_rate + z*RD
             LR = np.clip(LR, 0, 1)
             expectation += LR * error
-    if model_type == 'RLzx':
+            expectations.append(expectation)
+    if model_type == 'DGzk':
         z = params[0]
-        b = params[1]
-        learning_rates = [1, b, b, b]
+        k = params[1]
         subdata = human.query("trial==@trial & stage<=@stage")
         observations = subdata['color'].to_numpy()
         RDs = subdata['RD'].to_numpy()
         expectation = 0
-        for o, obs in enumerate(observations):
-            stg = int(subdata.iloc[o]['stage'])
-            zRD = 1 if stg<2 else z*RDs[o]
-            learning_rate = learning_rates[stg]
-            error = obs - expectation
-            LR = learning_rate * zRD
-            LR = np.clip(LR, 0, 1)
-            expectation += LR * error
-    if model_type in ['Z0K1', 'Z', 'K', 'ZK']:
-        if model_type=='Z0K1':
-            z = 0
-            k = 1
-        if model_type == 'Z':
-            z = params[0]
-            k = 1
-        if model_type=='K':
-            z = 0
-            k = params[0]
-        if model_type=='ZK':
-            z = params[0]
-            k = params[1]
-        subdata = human.query("trial==@trial & stage<=@stage")
-        observations = subdata['color'].to_numpy()
-        RDs = subdata['RD'].to_numpy()
-        expectation = 0
+        expectations = []
         for o, obs in enumerate(observations):
             stg = int(subdata.iloc[o]['stage'])
             decay = 1 / (o+1)
@@ -140,75 +77,8 @@ def get_expectation(model_type, params, trial, stage, sid):
             weight = decay**k + z*RD
             weight = np.clip(weight, 0, 1)
             expectation += weight * error
-    if model_type == 'ZxK':
-        z = params[0]
-        k = params[1]
-        subdata = human.query("trial==@trial & stage<=@stage")
-        observations = subdata['color'].to_numpy()
-        RDs = subdata['RD'].to_numpy()
-        expectation = 0
-        for o, obs in enumerate(observations):
-            stg = int(subdata.iloc[o]['stage'])
-            decay = 1 / (o+1)
-            zRD = 1 if stg in [0,1] else z*RDs[o]
-            error = obs - expectation
-            weight = decay**k * zRD
-            weight = np.clip(weight, 0, 1)
-            expectation += weight * error
-    if model_type=='ZS':
-        decays = [1, params[0], params[1], params[2]]
-        z = params[3]
-        subdata = human.query("trial==@trial & stage<=@stage")
-        observations = subdata['color'].to_numpy()
-        RDs = subdata['RD'].to_numpy()
-        expectation = 0
-        neighbors = len(subdata['who'].unique()) - 1
-        for o, obs in enumerate(observations):
-            stg = int(subdata.iloc[o]['stage'])
-            # decay = 1 if stg==0 else 1/(stg*neighbors)
-            decay = decays[stg]
-            RD = 0 if stg in [0,1] else RDs[o]
-            error = obs - expectation
-            weight = decay + z*RD
-            weight = np.clip(weight, 0, 1)
-            expectation += weight * error
-        # print(stage, decay, observations, expectation)
-    if model_type in ['DGn', 'DGrd', 'DGrds', 'DGrdp', 'DGrdpz']:
-        subdata = human.query("trial==@trial & stage<=@stage")
-        observations = subdata['color'].to_numpy()
-        if model_type=='DGn':
-            expectation = np.mean(observations)
-        if model_type in ['DGrd', 'DGrds']:
-            if model_type == 'DGrd':
-                late_weights = [1,1]
-            if model_type == 'DGrds':
-                late_weights = [params[0], params[1]]                
-            if stage in [0,1]:
-                expectation = np.mean(observations)
-            else:
-                early_data = human.query("trial==@trial & stage<=1")
-                early_observations = early_data['color'].to_numpy()       
-                expectation = np.mean(early_observations)
-                late_data = human.query("trial==@trial & stage>1")
-                late_observations = late_data['color'].to_numpy()
-                RDs = late_data['RD'].to_numpy()
-                for o, obs in enumerate(late_observations):
-                    stg = 0 if int(late_data.iloc[o]['stage'])==2 else 1
-                    w = late_weights[stg]
-                    RD = RDs[o]
-                    # weight = np.clip(w*RD, 0, 1)
-                    expectation += w*RD*obs
-        if model_type in ['DGrdp', 'DGrdpz']:
-            RDs = subdata['RD'].to_numpy()
-            w = 1 if model_type=='DGrdp' else params[0]
-            weights = []
-            for o, obs in enumerate(observations):
-                stg = int(subdata.iloc[o]['stage'])
-                RD = RDs[o] if stg>1 else 0
-                weights.append(1+w*RD)  # weight of observation is 1 by default, but 1+RD for stage 2-3
-            expectation = np.mean(weights*observations) # average weighted observations
-    return expectation
-
+            expectations.append(expectation)
+    return expectations
 
 def likelihood(params, model_type, sid):
     NLL = 0
@@ -218,92 +88,40 @@ def likelihood(params, model_type, sid):
     inv_temp = params[-1]
     for trial in trials:
         for stage in stages:
-            expectation = get_expectation(model_type, params, trial, stage, sid)
+            expectations = get_expectations(model_type, params, trial, stage, sid)
+            final_expectation = expectations[-1]
             act = human.query("trial==@trial and stage==@stage")['action'].unique()[0]
-            prob = scipy.special.expit(inv_temp*expectation)
+            prob = scipy.special.expit(inv_temp*final_expectation)
             NLL -= np.log(prob) if act==1 else np.log(1-prob)
     return NLL
 
-def stat_fit_scipy(model_type, sid, save=True):
-    if model_type == 'RLz':
-        param0 = [0.1, 0.1, 1.0]
-        bounds = [(0,2), (0,1), (0,10)]
-    if model_type == 'RLzx':
-        param0 = [1, 0.1, 1.0]
-        bounds = [(0,10), (0,1), (0,10)]
-    if model_type == 'RL1':
-        param0 = [0.1, 1.0]
-        bounds = [(0,1), (0,10)]
-    if model_type == 'RL3':
-        param0 = [0.1, 0.1, 0.1, 1.0]
-        bounds = [(0,1), (0,1), (0, 1), (0,10)]
-    if model_type == 'RL3rd':
-        param0 = [0.5, 0.5, 0.5, 1.0]
-        bounds = [(0,10), (0,10), (0, 10), (0,10)]
-    if model_type == 'Z0K1':
-        param0 = [1.0]
-        bounds = [(0,10)]
-    if model_type == 'Z':
-        param0 = [0.5, 1.0]
-        bounds = [(0,2), (0,10)]
-    if model_type == 'K':
-        param0 = [1.0, 1.0]
-        bounds = [(0.5,2.0), (0,10)]
-    if model_type == 'ZK':
-        param0 = [0.5, 1.0, 1.0]
-        bounds = [(0,2), (0.1,2), (0,30)]
-    if model_type == 'ZxK':
-        param0 = [1, 1.0, 1.0]
-        bounds = [(0,10), (0.1,2), (0,10)]
-    if model_type == 'ZS':
-        param0 = [0.3, 0.2, 0.1, 0.5, 1.0]
-        bounds = [(0,1), (0,1), (0,1), (0,2), (0,10)]
-    if model_type == 'DGn':
-        param0 = [1.0]
-        bounds = [(0,10)]
-    if model_type == 'DGrd':
-        param0 = [1.0]
-        bounds = [(0,10)]
-    if model_type == 'DGrds':
-        param0 = [0.5, 0.5, 1]
-        bounds = [(0, 3), (0,3), (0,10)]
-    if model_type == 'DGrdp':
-        param0 = [1.0]
-        bounds = [(0,10)]
-    if model_type == 'DGrdpz':
-        param0 = [1.0, 1.0]
-        bounds = [(0.5, 2.0), (0,10)]
+def stat_fit_scipy(model_type, sid):
+    param0, bounds = get_param_init_bounds(model_type)
     result = scipy.optimize.minimize(
         fun=likelihood,
         x0=param0,
         args=(model_type, sid),
         bounds=bounds,
         options={'disp':False})
-    # Save Negative Log Likelihood and McFadden R2
     NLL = result.fun
     mcfadden_r2 = compute_mcfadden(NLL, sid)
+    # Save Results and Best Fit Parameters
     performance_data = pd.DataFrame([[model_type, sid, NLL, mcfadden_r2]], columns=['type', 'sid', 'NLL', 'McFadden R2'])
-    if save:
-        performance_data.to_pickle(f"data/{model_type}_{sid}_performance.pkl")
-    # Save the fitted parameters
+    performance_data.to_pickle(f"data/{model_type}_{sid}_performance.pkl")
     param_names = get_param_names(model_type)
     params = list(result.x)
     params.insert(0, sid)
     params.insert(0, model_type)
     fitted_params = pd.DataFrame([params], columns=param_names)
-    if save:
-        fitted_params.to_pickle(f"data/{model_type}_{sid}_params.pkl")
+    fitted_params.to_pickle(f"data/{model_type}_{sid}_params.pkl")
     return performance_data, fitted_params
 
 if __name__ == '__main__':
-
     model_type = sys.argv[1]
     sid = int(sys.argv[2])
-
     start = time.time()
     print(f"fitting {model_type}, {sid}")
     performance_data, fitted_params = stat_fit_scipy(model_type, sid)
-
     print(performance_data)
     print(fitted_params)
     end = time.time()
--- rerun.py
+++ rerun.py
@@ -4,27 +4,28 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import sys
-from fit import get_expectation
+from fit import get_expectations
 import time
 
-def rerun(model_type, sid, save=True, seed=0):
+def rerun(model_type, sid, seed=0):
 	rng = np.random.RandomState(seed=seed)
 	human = pd.read_pickle(f"data/human.pkl").query("sid==@sid")
 	trials = human['trial'].unique()
 	stages = human['stage'].unique()
-	params = pd.read_pickle(f"data/{model_type}_{sid}_params.pkl").loc[0].to_numpy()[1:]
+	params = pd.read_pickle(f"data/{model_type}_{sid}_params.pkl").loc[0].to_numpy()[2:]
+	inv_temp = params[-1]
 	dfs = []
-	columns = ['type', 'sid', 'trial', 'network', 'stage', 'who', 'color', 'degree', 'RD', 'action']
+	columns = ['type', 'sid', 'trial', 'network', 'stage', 'who', 'color', 'degree', 'RD', 'action', 'expectation']
 	for trial in trials:
 		for stage in stages:
-			# update expectation based on model type
-			expectation = get_expectation(model_type, params, trial, stage, sid)
-			# generate one row per observation, even though 'action' will be the same
-			inv_temp = params[-1]
-			prob = scipy.special.expit(inv_temp*expectation)
+			expectations = get_expectations(model_type, params, trial, stage, sid)
+			final_expectation = expectations[-1]
+			prob = scipy.special.expit(inv_temp*final_expectation)
 			action = 1 if rng.uniform(0,1) < prob else -1
 			subdata = human.query("trial==@trial & stage==@stage")
 			observations = subdata['color'].to_numpy()
+			# NOTE: action and expectation will be identical for all observations within a stage
+			# (that is, they are the 'final' expectation and action for this stage)
 			for o, obs in enumerate(observations):
 				network = subdata['network'].unique()[0]
 				who = subdata['who'].to_numpy()[o]
@@ -41,23 +42,20 @@ def rerun(model_type, sid, save=True, seed=0):
 					color,
 					degree,
 					RD,
-					action
+					action,
+					final_expectation
 				]], columns=columns)
 				dfs.append(df)
 	rerun_data = pd.concat(dfs, ignore_index=True)
-	if save:
-		rerun_data.to_pickle(f"data/{model_type}_{sid}_rerun.pkl")
+	rerun_data.to_pickle(f"data/{model_type}_{sid}_rerun.pkl")
 	return rerun_data
 
 
 if __name__ == '__main__':
-
 	model_type = sys.argv[1]
 	sid = int(sys.argv[2])
-
 	start = time.time()
 	choice_data = rerun(model_type, sid)
-
 	print(choice_data)
 	end = time.time()
 	print(f"runtime {(end-start)/60:.4} min")
