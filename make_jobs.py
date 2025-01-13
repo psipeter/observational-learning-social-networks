6d10a71d91b9556118fac988fb9e657334369b4a
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-13 12:58:48 -0500

    remove k argv from make_jobs

--- make_jobs.py
+++ make_jobs.py
@@ -10,9 +10,8 @@ for sid in sids:
       paramfile = sys.argv[2]
       params = pd.read_pickle(f"data/{paramfile}_{sid}_params.pkl")
       z = params['z'].unique()[0] if sys.argv[3]=='load' else float(sys.argv[3])
-      k = params['k'].unique()[0] if sys.argv[4]=='load' else float(sys.argv[4])
-      inv_temp = params['inv_temp'].unique()[0] if sys.argv[5]=='load' else float(sys.argv[5])
-      fit_string = f"python run.py {model_type} {sid} {paramfile} {z} {k} {inv_temp}"
+      inv_temp = params['inv_temp'].unique()[0] if sys.argv[4]=='load' else float(sys.argv[4])
+      fit_string = f"python run.py {model_type} {sid} {paramfile} {z} {inv_temp}"
       rerun_string = f"python rerun.py {model_type} {sid}"
       file_string = f'nef_{sid}.sh'
    elif model_type in ['NEF_RL']:
