647f16e4fc86fd8051762e288fd046e36bd5c8d2
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-29 17:28:41 -0500

    typo

--- NEF_WM.py
+++ NEF_WM.py
@@ -194,7 +194,7 @@ def run_WM(dataset, sid, z, save=True):
         elif dataset=='carrabin':
             n_neurons = 300
             seed_net = sid + 1000*trial
-        net, sim = simulate_WM(env=env, seed_net=sid, z=z, progress_bar=False)
+        net, sim = simulate_WM(env=env, n_neurons=n_neurons, seed_net=seed_net, z=z, progress_bar=False)
         n_observations = 0
         for stage in env.stages:
             subdata = empirical.query("trial==@trial and stage==@stage")
