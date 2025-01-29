7a8f75d43460c8b1c2a9f3423db94b5d2c0309d1
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-29 10:37:29 -0500

    comment out neural activity and noise for data collection

--- collect_jobs.py
+++ collect_jobs.py
@@ -13,40 +13,40 @@ elif dataset=='jiang':
 dfs1 = []
 dfs2 = []
 dfs3 = []
-dfs4 = []
-dfs5 = []
+# dfs4 = []
+# dfs5 = []
 
 for sid in sids:
 	try:
 		df1 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_performance.pkl")
 		df2 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_params.pkl")
 		df3 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_dynamics.pkl")
-		if model_type in ['NEF_WM', 'NEF_RL']: 
-			df4 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_activities.pkl")
-		if noise:
-			df5 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_noise.pkl")
+		# if model_type in ['NEF_WM', 'NEF_RL']: 
+			# df4 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_activities.pkl")
+		# if noise:
+		# 	df5 = pd.read_pickle(f"data/{model_type}_{dataset}_{sid}_noise.pkl")
 		dfs1.append(df1)
 		dfs2.append(df2)
 		dfs3.append(df3)
-		if model_type in ['NEF_WM', 'NEF_RL']: 
-			dfs4.append(df4)
-		if noise:
-			dfs5.append(df5)
+		# if model_type in ['NEF_WM', 'NEF_RL']: 
+			# dfs4.append(df4)
+		# if noise:
+		# 	dfs5.append(df5)
 	except:
 		print(f"missing {model_type} {sid}")
 
 performance = pd.concat(dfs1, ignore_index=True)
 params = pd.concat(dfs2, ignore_index=True)
 reruns = pd.concat(dfs3, ignore_index=True)
-if model_type in ['NEF_WM', 'NEF_RL']: 
-	activities = pd.concat(dfs4, ignore_index=True)
-if noise:
-	noise_reruns = pd.concat(dfs5, ignore_index=True)
+# if model_type in ['NEF_WM', 'NEF_RL']: 
+	# activities = pd.concat(dfs4, ignore_index=True)
+# if noise:
+# 	noise_reruns = pd.concat(dfs5, ignore_index=True)
 
 performance.to_pickle(f"data/{model_type}_{dataset}_{label}_performance.pkl")
 params.to_pickle(f"data/{model_type}_{dataset}_{label}_params.pkl")
 reruns.to_pickle(f"data/{model_type}_{dataset}_{label}_dynamics.pkl")
-if model_type in ['NEF_WM', 'NEF_RL']: 
-	activities.to_pickle(f"data/{model_type}_{dataset}_{label}_activities.pkl")
-if noise:
-	noise_reruns.to_pickle(f"data/{model_type}_{dataset}_{label}_noise.pkl")
+# if model_type in ['NEF_WM', 'NEF_RL']: 
+# 	activities.to_pickle(f"data/{model_type}_{dataset}_{label}_activities.pkl")
+# if noise:
+# 	noise_reruns.to_pickle(f"data/{model_type}_{dataset}_{label}_noise.pkl")
