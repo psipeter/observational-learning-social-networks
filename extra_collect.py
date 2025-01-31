f2a9e1563cb5e45bc722ae6b5257c4397fa199fe
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-31 14:48:12 -0500

    collect noise

--- extra_collect.py
+++ extra_collect.py
@@ -14,3 +14,9 @@ if experiment=='variance_LR':
 		dfs.append(pd.read_pickle(f"data/NEF_RL_variance_LR_carrabin_{sid}_{n}.pkl"))
 	alpha_data = pd.concat(dfs, ignore_index=True)
 	alpha_data.to_pickle(f"data/NEF_RL_variance_LR_carrabin_alpha_{label}.pkl")
+if experiment=='noise_WM':
+	for n1 in n_neurons:
+		for n2 in n_neurons:
+			dfs.append(pd.read_pickle(f"data/NEF_WM_noise_WM_carrabin_{sid}_{n1}_{n2}.pkl"))
+	noise_data = pd.concat(dfs, ignore_index=True)
+	noise_data.to_pickle(f"data/NEF_WM_noise_WM_carrabin_{label}.pkl")
