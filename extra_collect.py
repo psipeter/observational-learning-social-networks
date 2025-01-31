90545d074c6599bf4766f67c6c90e8414bc3cf7d
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-01-31 12:54:33 -0500

    typo

--- extra_collect.py
+++ extra_collect.py
@@ -11,6 +11,6 @@ n_neurons = [int(arg) for arg in sys.argv[4:]]
 dfs = []
 if experiment=='variance_LR':
 	for n in n_neurons:
-		dfs.append(pd.read_pickle(f"data/NEF_RL_variance_LR_carrabin_{sid}_{n_neurons}.pkl"))
+		dfs.append(pd.read_pickle(f"data/NEF_RL_variance_LR_carrabin_{sid}_{n}.pkl"))
 	alpha_data = pd.concat(dfs, ignore_index=True)
 	alpha_data.to_pickle(f"data/NEF_RL_variance_LR_carrabin_alpha_{label}.pkl")
