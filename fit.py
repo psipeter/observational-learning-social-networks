3210e11a7e6fc0a15074dae81077c00649c3db84
Peter Duggins <psipeter@hydra.dartmouth.edu>
2025-04-03 10:39:21 -0400

    typo

--- fit.py
+++ fit.py
@@ -70,30 +70,30 @@ def NEF_jiang_loss(trial, model_type, sid):
     NLL = NLL_loss([beta], model_type, sid)
     return NLL
 
-def NEF_carrabin_loss(trial, model_type, sid):
-    pass
-    # if model_type=='NEF_RL':
-    #     alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
-    #     n_all = trial.suggest_int("n_all", 10, 500, step=10)
-    #     lambd = trial.suggest_float("lambda", 1.0, 1.0, step=0.01)
-    #     data = run_RL("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_all, n_learning=n_all, n_error=n_all)
-    # if model_type=='NEF_WM':
-    #     alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
-    #     n_all = trial.suggest_int("n_all", 10, 500, step=10)
-    #     lambd = trial.suggest_float("lambda", 1.0, 1.0, step=0.01)
-    #     data = run_WM("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_memory=n_all, n_neurons=n_all, n_error=n_all)
-    # if model_type=='NEF_syn':
-    #     alpha = trial.suggest_float("alpha", 1e-5, 1e-3, step=1e-5)
-    #     n_neurons = trial.suggest_int("n_neurons", 10, 1000, step=10)
-    #     lambd = trial.suggest_float("lambda", 0.0, 0.0, step=0.01)
-    #     data = run_NEF_syn("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_neurons)
-    # if model_type=='NEF_rec':
-    #     alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
-    #     n_neurons = trial.suggest_int("n_neurons", 10, 1000, step=10)
-    #     lambd = trial.suggest_float("lambda", 0.0, 0.0, step=0.01)
-    #     data = run_NEF_rec("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_neurons)
-    # loss = QID_loss([], model_type, sid)
-    return loss
+# def NEF_carrabin_loss(trial, model_type, sid):
+#     pass
+#     if model_type=='NEF_RL':
+#         alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
+#         n_all = trial.suggest_int("n_all", 10, 500, step=10)
+#         lambd = trial.suggest_float("lambda", 1.0, 1.0, step=0.01)
+#         data = run_RL("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_all, n_learning=n_all, n_error=n_all)
+#     if model_type=='NEF_WM':
+#         alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
+#         n_all = trial.suggest_int("n_all", 10, 500, step=10)
+#         lambd = trial.suggest_float("lambda", 1.0, 1.0, step=0.01)
+#         data = run_WM("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_memory=n_all, n_neurons=n_all, n_error=n_all)
+#     if model_type=='NEF_syn':
+#         alpha = trial.suggest_float("alpha", 1e-5, 1e-3, step=1e-5)
+#         n_neurons = trial.suggest_int("n_neurons", 10, 1000, step=10)
+#         lambd = trial.suggest_float("lambda", 0.0, 0.0, step=0.01)
+#         data = run_NEF_syn("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_neurons)
+#     if model_type=='NEF_rec':
+#         alpha = trial.suggest_float("alpha", 0.01, 1.0, step=0.01)
+#         n_neurons = trial.suggest_int("n_neurons", 10, 1000, step=10)
+#         lambd = trial.suggest_float("lambda", 0.0, 0.0, step=0.01)
+#         data = run_NEF_rec("carrabin", sid, alpha=alpha, z=0, lambd=lambd, n_neurons=n_neurons)
+#     loss = QID_loss([], model_type, sid)
+#     return loss
 
 def math_carrabin_loss(trial, model_type, sid):
     if model_type=='RL_n':
